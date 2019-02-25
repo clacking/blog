b11515cb78df90a9e081d3a0a1718522f7028c57
clacking <click@clacking.net>
2019-02-25 14:43:40 +0900

    change aboutme

--- src/pages/archive/uni.js
+++ src/pages/archive/uni.js
@@ -8,14 +8,12 @@ export default function Uni () {
     return (
         <Layout title={'Archive: Uni project'}>
             <SEO title={'archive'} />
-            <>
-                <p>大学での卒業研究で開発したシステムです。</p>
-                <p></p>
-                <ul>
-                    <li>Git: tbd</li>
-                    <li>Poster: <a href={withPrefix('files/nwtn_pstr_public.pdf')}>PDF</a></li>
-                </ul>
-            </>
+            <p>大学での卒業研究で開発したシステムです。</p>
+            <p>Keywords: Node.JS, ReactJS, WebSocket, IoT, ESP32, Automation, Amazon Alexa, Python, OpenCV</p>
+            <ul>
+                <li>Git: tbd</li>
+                <li>Poster: <a href={withPrefix('files/nwtn_pstr_public.pdf')}>PDF</a></li>
+            </ul>
         </Layout>
     )
 }
