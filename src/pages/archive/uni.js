cc7526fae26618b4f371772c1993bf07528e0b7d
clacking <click@clacking.net>
2019-03-09 03:40:10 +0900

    change about

--- src/pages/archive/uni.js
+++ src/pages/archive/uni.js
@@ -11,7 +11,7 @@ export default function Uni () {
             <p>大学での卒業研究で開発したシステムです。</p>
             <p>Keywords: Node.JS, ReactJS, WebSocket, IoT, ESP32, Automation, Amazon Alexa, Python, OpenCV</p>
             <ul>
-                <li>Git: tbd</li>
+                <li>Git: <a href='https://github.com/clacking/sh'>Github</a></li>
                 <li>Poster: <a href={withPrefix('files/nwtn_pstr_public.pdf')}>PDF</a></li>
             </ul>
             <h3>Links</h3>
