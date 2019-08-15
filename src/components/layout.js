29d583613dde65d3193f70b8320e058d06fec155
clacking <click@clacking.net>
2019-08-15 14:11:22 +0900

    layout change

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ class Layout extends React.Component {
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
-            maxWidth: rhythm(32),
+            maxWidth: `69rem`,
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
